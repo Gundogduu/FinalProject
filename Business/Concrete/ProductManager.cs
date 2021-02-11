a0ca033524a9ff5144c0efd8b5d9afc80ee515f6
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-02-11 21:09:57 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        //bağımlı yapmamak için IProductDal verdik.
+        //artık InMemory de olabilir, Entity Framework de olabilir yarın başka birşey de olabilir.Kolayca ekleyebiliriz.
+        public ProductManager(IProductDal productDal)
+        {
+            this._productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //yetkisi var mı?
+
+            return _productDal.GetAll();
+        }
+    }
+}
+//dikkat business da ınMemory,entityFramework hiç bir şey yok.
+//Business'ın bildiği tek şey IProductDal
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{   //bir class'ın default erişim bildirgeci internal'dir.Internal sadece Entities erişebilir demek.
+    public class Product: IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
